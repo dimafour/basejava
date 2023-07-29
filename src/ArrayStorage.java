3ca1fd84a8ba334fbcf46049104f0e4698711cf8
Dmitrii Drachev <xdd100500@gmail.com>
2023-07-29 17:54:55 +0300

    Implement ArrayStorage methods

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,47 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size(); j++) {
+                    storage[j] = storage[j + 1];
+                }
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] storageTrimmed = new Resume[size()];
+        System.arraycopy(storage, 0, storageTrimmed, 0, size());
+        return storageTrimmed;
     }
 
     int size() {
-        return 0;
+        int i;
+        for (i = 0; i < storage.length; i++) {
+            if (storage[i] == null) return i;
+        }
+        return i;
     }
 }
