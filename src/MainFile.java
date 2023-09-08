b1d6c34ec7806ebd976b815e253877be004ac747
Drachev Dmitrii <xdd100500@gmail.com>
2023-09-08 14:04:49 +0300

    Refactor printFiles() method in MainFile

--- src/MainFile.java
+++ src/MainFile.java
@@ -2,25 +2,29 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class MainFile {
+    static StringBuilder sb = new StringBuilder();
 
     public static ArrayList<String> allFilesList = new ArrayList<>();
 
     public static void main(String[] args) {
-        addFiles(new File("C:\\Projects\\basejava\\src"));
-        System.out.println(allFilesList);
+        printFiles(new File("C:\\Projects\\basejava\\src"));
+//        System.out.println(allFilesList);
     }
 
-    public static void addFiles(File file) {
+    public static void printFiles(File file) {
         String[] list = file.list();
         if (list != null) {
             for (String name : list) {
                 File element = new File(file.getAbsolutePath(), name);
                 if (element.isDirectory()) {
-                    addFiles(element);
+                    System.out.print(sb.append("  "));
+                    System.out.println(element + ": " + sb.append("  "));
+                    printFiles(element);
                 } else {
-                    allFilesList.add(name);
+                    System.out.println(sb + name);
                 }
             }
         }
+        sb.delete(0, sb.length());
     }
 }
